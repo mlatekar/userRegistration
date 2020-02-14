cbe9eda3cc5eb00314cbca9d8b50132df572918a
mangesh <mlatekar@gmail.com>
2020-02-14 11:46:12 +0530

    ADDED:Valid last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,9 +1,9 @@
 echo "Welcome to User Registration"
 shopt -s extglob
-echo "Read user name"
-read userName
+read -p  "Read User name & last name : " userName userLastName
 name="^[A-Z]{1}[a-zA-Z]{3,}$"
-if [[ $userName =~ $name ]]
+lastName="^[A-Z]{1}[a-zA-Z]{3,}$"
+if [[ $userName =~ $name && $userLastName =~ $lastName ]]
 then
 	echo "Valid"
 else
