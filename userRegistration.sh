9292b3f970d2a367de67d5986cacab3920f93764
mangesh <mlatekar@gmail.com>
2020-02-14 14:33:09 +0530

    ADDED:check valid mobile number

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,19 +1,45 @@
 echo "Welcome to User Registration"
+
 shopt -s extglob
-read -p  "Read User name & last name : " userName userLastName 
-read -p "Enter you Email address: " userEmail
+
 name="^[A-Z]{1}[a-zA-Z]{3,}$"
 lastName="^[A-Z]{1}[a-zA-Z]{3,}$"
 email="^[a-zA-Z]{3,}[.]*{1}[a-zA-Z]*{3,}[@]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,3}[.]*{1}[a-zA-Z]*{2,3}$"
+mobileNumber="^[0-9]{2}[[:space:]][0-9]{10}$"
+
+function validName()
+{
 if [[ $userName =~ $name && $userLastName =~ $lastName ]]
 then
-	echo "Valid"
+	echo "Valid Name and Last Name "
 else
-	echo "Invalid"
+	echo "Invalid first letter must be capital "
 fi
+}
+
+function validEmailAddress()
+{
 if [[ $userEmail =~ $email ]]
 then 
-echo "Valid"
+echo "Valid Email Address"
 else
-echo "Invalid"
+echo "Invalid Email Address"
 fi
+}
+
+function validmobileNumber()
+{
+if [[ $countryCodeMobileNumber =~ $mobileNumber ]]
+then
+	echo "Valid Mobile Number"
+else
+	echo "Invalid Mobile Number"
+fi
+}
+read -p  "Read User name & last name : " userName userLastName 
+validName
+read -p "Enter you Email address: " userEmail
+validEmailAddress
+read -p "Enter country code with mobile number : " countryCodeMobileNumber
+validMobileNumber
+
