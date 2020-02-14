18b78816fcdad3e42e1aa3fc80b66e265d60507c
mangesh <mlatekar@gmail.com>
2020-02-14 15:10:24 +0530

    ADDED:password with 8 characters

--- userRegistration.sh
+++ userRegistration.sh
@@ -6,7 +6,7 @@ name="^[A-Z]{1}[a-zA-Z]{3,}$"
 lastName="^[A-Z]{1}[a-zA-Z]{3,}$"
 email="^[a-zA-Z]{3,}[.]*{1}[a-zA-Z]*{3,}[@]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,3}[.]*{1}[a-zA-Z]*{2,3}$"
 mobileNumber="^[0-9]{2}[[:space:]][0-9]{10}$"
-
+password="^[a-zA-Z]{8}$"
 function validName()
 {
 if [[ $userName =~ $name && $userLastName =~ $lastName ]]
@@ -27,7 +27,7 @@ echo "Invalid Email Address"
 fi
 }
 
-function validmobileNumber()
+function validMobileNumber()
 {
 if [[ $countryCodeMobileNumber =~ $mobileNumber ]]
 then
@@ -36,10 +36,20 @@ else
 	echo "Invalid Mobile Number"
 fi
 }
+function validPassword
+{
+if [[ $enterPassword  =~ $password ]]
+then
+   echo "Valid password"
+else
+   echo "Invalid password"
+fi
+}
 read -p  "Read User name & last name : " userName userLastName 
 validName
 read -p "Enter you Email address: " userEmail
 validEmailAddress
 read -p "Enter country code with mobile number : " countryCodeMobileNumber
 validMobileNumber
-
+read -p "Enter your Password : " enterPassword
+validPassword
