741ad31fb89b141a08ce65d2e2dbac08228883d0
mangesh <mlatekar@gmail.com>
2020-02-14 12:41:22 +0530

    ADDED:valid email address

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,11 +1,19 @@
 echo "Welcome to User Registration"
 shopt -s extglob
-read -p  "Read User name & last name : " userName userLastName
+read -p  "Read User name & last name : " userName userLastName 
+read -p "Enter you Email address: " userEmail
 name="^[A-Z]{1}[a-zA-Z]{3,}$"
 lastName="^[A-Z]{1}[a-zA-Z]{3,}$"
+email="^[a-zA-Z]{3,}[.]*{1}[a-zA-Z]*{3,}[@]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,3}[.]*{1}[a-zA-Z]*{2,3}$"
 if [[ $userName =~ $name && $userLastName =~ $lastName ]]
 then
 	echo "Valid"
 else
 	echo "Invalid"
+fi
+if [[ $userEmail =~ $email ]]
+then 
+echo "Valid"
+else
+echo "Invalid"
 fi
